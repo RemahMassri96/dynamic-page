4c4c877a4d29ea61b35cfdc44ea16205f8a20c5b
remah massri <remah.massri@gmail.com>
2020-05-02 19:48:39 +0300

    add functions

--- js/dynamic.js
+++ js/dynamic.js
@@ -2,23 +2,43 @@
 
 var message = "Hi there! Click OK to continue.";
 alert(message);
- 
 /* The following line won't execute until you dismiss previous alert */
 alert("This is another alert box.");
 
-var result = confirm("Are you sure?");
+printName();
+userOrder();
+
+function pressConfirm(){
+    var result = confirm("Are you sure?");
  
-if(result) {
-    document.write("You clicked OK button!");
-} else {
-    document.write("You clicked Cancel button!");
+    if(result) {
+        document.write("You clicked OK button!");
+    } else {
+        document.write("You clicked Cancel button!");
+    }
+    
 }
 
-
-var name = prompt("What's your name?");
+function printName(){
+    var name = prompt("What's your name?");
  
-if(name.length > 0 && name != "null") {
-    document.write("Hi, " + name);
-} else {
-    document.write("Anonymous!");
+    if(name.length > 0 && name != "null") {
+        document.write("Hi, " + name);
+    } else {
+        document.write("Anonymous!");
+    }
+}
+
+function userOrder (){
+    var userOrder = prompt(' what would you like to order ? flower or coffe');
+    var item ='';
+
+    if(userOrder === 'flower'){
+        document.write('<img src="https://images.unsplash.com/photo-1526047932273-341f2a7631f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"');
+
+    }else if (userOrder === 'coffe'){
+        document.write('<img src="https://static.turbosquid.com/Preview/2014/07/07__23_41_37/coffe_cup_preview_01.jpg6e737ca4-a947-4afb-8073-7748907cba17DefaultHQ.jpg"');
+    }
+
+
 }
